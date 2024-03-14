dce7d9715ad3b27537468159116917cb119a7900
FaisalRichardJr <faisalrichardjr@gmail.com>
2024-03-14 17:33:34 +0800

    Flask shell changes

--- ms_inventory_system.py
+++ ms_inventory_system.py
@@ -1,1 +1,13 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import Role, User, Login, TransactionType, Transaction, ProductType, Product, Stock, Order
+
+
+# Registers the items to the python interpreter session, making it not necessary to import everything in python
+# interpreter manually (when ran using the flask shell command).
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'Role': Role, 'User': User, 'Login': Login,
+            'TransactionType': TransactionType, 'Transaction': Transaction, 'ProductType': ProductType,
+            'Product': Product, 'Stock': Stock, 'Order': Order}
